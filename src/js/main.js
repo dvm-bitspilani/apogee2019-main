34360d87350f5ade2371c965e479450d8d015a55
kunal-mohta <kunalmohta1818@gmail.com>
2019-02-03 21:04:38 +0530

    home footer visibility on scroll

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,18 @@ function init() {
         window.scrollTo(5,0);
     }
 
+    let homeFooterVisible = true;
+    let homeFooter = document.getElementsByClassName("home-footer")[0];
+    window.onscroll = () => {
+        if (homeFooterVisible && window.scrollY/window.innerHeight > 0.05) {
+            homeFooter.style.opacity = 0;
+            homeFooterVisible = false;
+        }
+        else if (!homeFooterVisible && window.scrollY < 0.2) {
+            homeFooter.style.opacity = 1;
+            homeFooterVisible = true;
+        }
+    }
 
     document.getElementById("dev-navlink").addEventListener("click", function (e) {
         e.preventDefault();

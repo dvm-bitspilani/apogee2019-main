f23a52af60902d9538f67dc9eab212b4120c83e0
kunal-mohta <kunalmohta1818@gmail.com>
2019-02-04 21:27:20 +0530

    home footer opacity time increase, speakers arrow fix

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,6 @@ function init() {
         let loader = document.getElementById("loader-wrap");
         loader.style.opacity = "0";
         setTimeout(() => loader.style.display = "none", 400);
-        window.scrollTo(5,0);
     }
 
     let homeFooterVisible = true;

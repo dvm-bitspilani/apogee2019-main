70356c57a7fbc1aa69eef1d0ea37e11dda51e5be
kunal-mohta <kunalmohta1818@gmail.com>
2019-02-10 22:27:22 +0530

    kernel event imgs

--- src/js/kernel-events.js
+++ src/js/kernel-events.js
@@ -5,3 +5,21 @@ window.addEventListener('load', () => {
     const e = document.querySelectorAll('.event-info p');
     Array.from(e).forEach(replaceText)
 });
+
+function init () {
+    const eventImgs = [
+        require('../imgs/kernel-events/armageddon.jpg'),
+        require('../imgs/kernel-events/icl.jpg'),
+        require('../imgs/kernel-events/raw.jpg'),
+        require('../imgs/kernel-events/robosoc.jpg'),
+        require('../imgs/kernel-events/sms.jpg'),
+        require('../imgs/kernel-events/ml.png'),
+        require('../imgs/kernel-events/junky.jpg'),
+        require('../imgs/kernel-events/reveng.jpg'),
+        require('../imgs/kernel-events/aic.png'),
+        require('../imgs/kernel-events/qed.jpg'),
+        require('../imgs/kernel-events/bplan.jpg'),
+    ];
+}
+
+init();
